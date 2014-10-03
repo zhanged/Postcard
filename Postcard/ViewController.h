b2dae67d4befbabcaf2c39998fac48aead8da0f0
zhanged <edison.m.zhang@gmail.com>
2014-10-03 16:59:01 -0700

    Initial Commit

---
+++ Postcard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Postcard
+//
+//  Created by Edison Zhang on 10/3/14.
+//  Copyright (c) 2014 Edison Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Postcard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Postcard
+//
+//  Created by Edison Zhang on 10/3/14.
+//  Copyright (c) 2014 Edison Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
